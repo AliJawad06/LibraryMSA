516caa046f5c0753bcb69f51f965a512b0abf8ad
Ali Jawad <ajawad@ad.unc.edu>
2023-11-28 16:05:25 -0500

    changed Header

--- src/views/header.js
+++ src/views/header.js
@@ -1,14 +1,11 @@
 import { useState } from 'react';
 import { Container, Group, Burger } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { MantineLogo } from '@mantinex/mantine-logo';
 import classes from './HeaderSimple.module.css';
 
 const links = [
-  { link: '/about', label: 'Features' },
-  { link: '/pricing', label: 'Pricing' },
-  { link: '/learn', label: 'Learn' },
-  { link: '/community', label: 'Community' },
+  { link: '/', label: 'Home' },
+  { link: '/books', label: 'Books' },
 ];
 
 export function HeaderSimple() {
