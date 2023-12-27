513a7d5522590badd04d48ae0454deec6981a9db
Ali Jawad <ajawad@ad.unc.edu>
2023-12-27 12:57:41 -0500

    fixed header

--- frontend/src/views/header.js
+++ frontend/src/views/header.js
@@ -1,22 +1,27 @@
 import { useState } from 'react';
 import { Container, Group, Burger } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { MantineLogo } from '@mantinex/mantine-logo';
 import classes from './HeaderSimple.module.css';
-import { API_URL } from '../shared/url';
 
 const links = [
-  { link: '/', label: 'Home' },
   { link: '/books', label: 'Books' },
+  { link: '/', label: 'Home' },
 ];
 
 export function HeaderSimple() {
-  const [opened, { toggle}] = useDisclosure(false);
+  const [opened, { toggle }] = useDisclosure(false);
+  const [active, setActive] = useState(links[0].link);
 
   const items = links.map((link) => (
     <a
+      key={link.label}
       href={link.link}
+      className={classes.link}
+      data-active={active === link.link || undefined}
       onClick={(event) => {
         event.preventDefault();
+        setActive(link.link);
         window.location.href = link.link
       }}
     >
@@ -26,9 +31,9 @@ export function HeaderSimple() {
 
   return (
     <header className={classes.header}>
-      <Container size="xxl" className={classes.inner}>
-        <img src='uncmsa-LOGO.png'/>
-        <Group gap={10} visibleFrom="xl">
+      <Container size="md" className={classes.inner}>
+      <img src='uncmsa-LOGO.png'/>
+        <Group gap={5} visibleFrom="xs">
           {items}
         </Group>
 
