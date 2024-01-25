a07814ef6dac5881112063b89c784196460f4b21
Ali Jawad <ajawad@ad.unc.edu>
2024-01-24 23:26:16 -0500

    fixed region

--- frontend/src/shared/amazonConfig/awsConfig.js
+++ frontend/src/shared/amazonConfig/awsConfig.js
@@ -1,6 +1,6 @@
 import { S3Client } from "@aws-sdk/client-s3";
 const awsConfig = {
-  region: " us-east-1", // Replace with your AWS region, e.g., "us-east-1"
+  region: "us-east-1", // Replace with your AWS region, e.g., "us-east-1"
   credentials: {
     accessKeyId: process.env.REACT_APP_AK,
     secretAccessKey: process.env.REACT_APP_SAK
