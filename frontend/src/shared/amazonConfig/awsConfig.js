64c8edc57e07e48e335df716ba240d9e83cdd07c
Ali Jawad <ajawad@ad.unc.edu>
2024-01-24 23:22:39 -0500

    keys

--- frontend/src/shared/amazonConfig/awsConfig.js
+++ frontend/src/shared/amazonConfig/awsConfig.js
@@ -1,10 +1,9 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import dotenv from 'dotenv';
 const awsConfig = {
   region: " us-east-1", // Replace with your AWS region, e.g., "us-east-1"
   credentials: {
-    accessKeyId: process.env.AK,
-    secretAccessKey: process.env.SAK
+    accessKeyId: process.env.REACT_APP_AK,
+    secretAccessKey: process.env.REACT_APP_SAK
   },
 };
 
