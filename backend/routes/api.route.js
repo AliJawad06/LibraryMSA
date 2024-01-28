bb380cc7b139e46915057648d7133726b14ad127
Ali Jawad <ajawad@ad.unc.edu>
2024-01-28 14:13:49 -0500

    testing

--- backend/routes/api.route.js
+++ backend/routes/api.route.js
@@ -126,7 +126,9 @@ router.route('/delete-checkout').post((req,res,next) =>{
 
 router.route('/getUserCheckoutsSize/:userId').get(  (req, res) => {
   const userId = req.params.userId;
+  console.log(userId)
   res.send(1)
+  
 
   // Find the user by their ID using promises
   /*userSchema.findById(userId)
