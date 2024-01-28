a4d2276b5873a0ed27a9521544bcd7b15ecac101
Ali Jawad <ajawad@ad.unc.edu>
2024-01-28 13:35:36 -0500

    changed cors settings

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const createError = require('http-errors');
 const userRoute = require('./routes/api.route')
 const dotenv = require('dotenv').config()
 const app = express();
-app.use(cors());
+app.use(cors({allowedHeaders: '*'}));
 
 // Express Route
 
