f9c992b0ad9fe6c091e47f6197be732ce133fe7d
Ali Jawad <ajawad@ad.unc.edu>
2024-09-07 13:53:41 -0400

    added links

--- frontend/src/views/resources.js
+++ frontend/src/views/resources.js
@@ -3,14 +3,14 @@ import classes from './resourcesStyle.module.css';
 
 export default function Resources() {
     const elements = [
-        {label: 'Maliki', links:[ 'https://malikischool.org/']},
-        {label: 'Aqeedah', links: ['https://bayhaqiyyah.com/']},
-        {label: 'Hanbali', links: ['https://www.almadrasahalhanbaliyyah.com/']},
-        {label: 'Everything', links: ['https://seekersguidance.org/']},
-        {label: 'Fiqh/Aqeedah/Tazkiya', links: ['https://www.arkview.org/']},
-        {label: 'History', links: ['https://muslimlegacy.com/']},
-        {label: 'Hanifi Foundations', links:[ 'https://archive.org/details/absolute-essentials-of-islam-faraz-fareed-rabbani','https://academy.seekersguidance.org/enrol/index.php?id=114']},
-        {label: "Shafi'i Foundations", links: ['https://drive.google.com/file/d/1srzua3PZKH_QlvO8cGoNEGS1CrqdfhMh/view?usp=sharing']}
+      {label: 'Hanifi Foundations', links:[ 'https://archive.org/details/absolute-essentials-of-islam-faraz-fareed-rabbani','https://academy.seekersguidance.org/enrol/index.php?id=114']},
+      {label: 'Everything', links: ['https://seekersguidance.org/']}, 
+      {label: "Shafi'i Foundations", links: ['https://drive.google.com/file/d/1srzua3PZKH_QlvO8cGoNEGS1CrqdfhMh/view?usp=sharing']},
+      {label: 'Maliki', links:[ 'https://malikischool.org/']},
+      {label: 'Aqeedah', links: ['https://bayhaqiyyah.com/']},
+      {label: 'Hanbali', links: ['https://www.almadrasahalhanbaliyyah.com/']},
+      {label: 'Fiqh/Aqeedah/Tazkiya', links: ['https://www.arkview.org/']},
+      {label: 'History', links: ['https://muslimlegacy.com/']}        
 
       ];
 
